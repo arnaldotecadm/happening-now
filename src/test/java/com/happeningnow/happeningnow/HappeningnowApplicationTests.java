67a0410f9e0e03810098e61963ad868e898e65b2
Alex Sander <sandermeneses2000@hotmail.com>
2023-11-06 20:29:58 +0000

    create the project using springboot

---
+++ src/test/java/com/happeningnow/happeningnow/HappeningnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happeningnow.happeningnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappeningnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
