95f69940b3335a3e489493a080b15ed65d4eb059
Alex Sander <sandermeneses2000@hotmail.com>
2023-11-10 16:03:20 +0000

    HN-3 <Create a dummy controller for a hello world, create a Postman collection and save the collection in resource folder>

---
+++ src/main/java/com/happeningnow/happeningnow/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.happeningnow.happeningnow;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String Application() {
+        return "Hello World from Spring Boot!!!";
+    }
+}
