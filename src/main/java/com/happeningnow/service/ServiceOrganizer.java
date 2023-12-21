f1ca89c9a95f84e2029258216480b10f8dd4b7e6
Alex Sander <sandermeneses2000@hotmail.com>
2023-12-21 13:11:12 +0000

    HN-10 Creation the Organizer service and repository

--- src/main/java/com/happeningnow/service/ServiceOrganizer.java
+++ src/main/java/com/happeningnow/service/ServiceOrganizer.java
@@ -1,4 +1,27 @@
-package com.happeningnow.services;
+package com.happeningnow.service;
 
+import com.happeningnow.model.Organizer;
+import com.happeningnow.repository.OrganizerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+
+@Service
 public class ServiceOrganizer {
+
+
+    private final OrganizerRepository organizerRepository;
+
+    public Page<Organizer> getOrganizer(){
+
+        Sort sort = Sort.by("name").ascending();
+        Pageable pageable = PageRequest.of(0, 50, sort);
+
+        return this.organizerRepository.findAll(pageable);
+    }
 }
