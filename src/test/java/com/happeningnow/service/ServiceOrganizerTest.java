96066a58f493b95ede666633a444cbe43d7c7541
Alex Sander <sandermeneses2000@hotmail.com>
2024-01-20 16:56:56 +0000

    HN-10 Creation the repository, service and test

--- src/test/java/com/happeningnow/service/ServiceOrganizerTest.java
+++ src/test/java/com/happeningnow/service/ServiceOrganizerTest.java
@@ -43,14 +43,41 @@ class ServiceOrganizerTest {
     }
 
     @Test
+    @DisplayName("Must find by id")
     void findById() {
-    }
+        //Arrange
+        UUID id = UUID.fromString("a0d3b612-cde9-417d-8c47-b268cc295e80");
+        Organizer mockOrganizer = mock(Organizer.class);
 
+        //Act
+        Mockito.when(mockOrganizer.getId()).thenReturn(id);
+        Mockito.when(serviceOrganizer.findById(mockOrganizer.getId())).thenReturn(Optional.of(organizer));
+        Mockito.when(organizer.getId()).thenReturn(id);
+
+        var result = organizer.getId();
+
+        //Assert
+        Assertions.assertEquals(result, mockOrganizer.getId());
+
+    }
     @Test
+    @DisplayName("Must find all organizer in page")
     void findAll() {
+        //Arrange
+
+        //Act
+
+        //Assert
     }
 
     @Test
+    @DisplayName("Must delete organizer")
     void delete() {
+        //Arrange
+
+        //Act
+
+        //Assert
+
     }
 }
