7db005f2c3458a2c7ab24038b9e404ae37137844
Alex Sander <sandermeneses2000@hotmail.com>
2024-02-29 11:18:19 +0000

    HN-11 Creation the repository, service and test

---
+++ src/test/java/com/happeningnow/service/ServiceArtistTest.java
@@ -0,0 +1,90 @@
+package com.happeningnow.service;
+
+import com.happeningnow.model.Artist;
+import com.happeningnow.repository.ArtistRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+import java.util.Collections;
+import java.util.UUID;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ServiceArtistTest {
+
+    @Autowired
+    private ServiceArtist serviceArtist;
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    private Artist artist;
+
+    @BeforeEach
+    public void createArtist(){
+        UUID id = UUID.fromString("a0d3b612-cde9-417d-8c47-b268cc295e80");
+        artist = new Artist(
+                id,"Alex Sander","Developer", Collections.emptyList()
+        );
+    }
+
+    @AfterEach
+    public void setUp(){
+        this.artistRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Must save a new artist")
+    public void save(){
+        var result = serviceArtist.save(artist);
+
+        var size = artistRepository.findAll().size();
+
+        Assertions.assertEquals(result.getName(), artist.getName());
+        Assertions.assertEquals(1, size, "The quantity of artist is different");
+    }
+
+    @Test
+    @DisplayName("Must find artist by id")
+    public void findById(){
+        serviceArtist.save(artist);
+
+        var result = serviceArtist.findById(artist.getId());
+
+        Assertions.assertEquals(result.get().getId(), artist.getId());
+    }
+
+    @Test
+    @DisplayName("Must find all artist")
+    public void artistList(){
+        serviceArtist.save(artist);
+
+        Page<Artist>listOfArtist =
+                serviceArtist.listArtist(PageRequest.of(0, 50, Sort.Direction.ASC, "Name"));
+
+        Assertions.assertFalse(listOfArtist.getContent().isEmpty());
+        Assertions.assertTrue(listOfArtist.stream().anyMatch(l -> l.getName().equals(artist.getName())));
+    }
+
+    @Test
+    @DisplayName("Must delete artist")
+    public void deleteById(){
+        serviceArtist.save(artist);
+
+        Page<Artist>listOfArtist =
+                serviceArtist.listArtist(PageRequest.of(0,50, Sort.Direction.ASC, "name"));
+
+        Assertions.assertFalse(listOfArtist.getContent().isEmpty());
+
+        serviceArtist.deleteById(artist.getId());
+
+        listOfArtist =
+                serviceArtist.listArtist(PageRequest.of(0,50, Sort.Direction.ASC, "name"));
+
+        Assertions.assertTrue(listOfArtist.getContent().isEmpty());
+    }
+}
