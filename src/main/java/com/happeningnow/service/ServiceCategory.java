16cf7c3416a6e5b181e17d5d51164f05a240ec40
Alex Sander Meneses Santos <74025683+alexsandermeneses@users.noreply.github.com>
2024-03-31 03:26:04 +0100

    HN-14 Creation the repository, service and test (#14)

---
+++ src/main/java/com/happeningnow/service/ServiceCategory.java
@@ -0,0 +1,13 @@
+package com.happeningnow.service;
+
+import com.happeningnow.model.Category;
+import com.happeningnow.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceCategory extends ServiceAbstract<Category, CategoryRepository> {
+
+    public ServiceCategory(CategoryRepository repository) {
+        super(repository);
+    }
+}
