5db8f7fe10cc78a187f0cb83ecf8f7839d3b081a
Alex Sander Meneses Santos <74025683+alexsandermeneses@users.noreply.github.com>
2024-04-02 12:38:27 +0100

    HN-15 Create the repository, service and test (#15)

---
+++ src/test/java/com/happeningnow/service/ServiceEventTest.java
@@ -0,0 +1,111 @@
+package com.happeningnow.service;
+
+import com.happeningnow.model.Event;
+import com.happeningnow.repository.EventRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.lang.reflect.Array;
+import java.time.*;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ServiceEventTest {
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    ServiceEvent serviceEvent;
+
+    private Event event;
+
+    @BeforeEach
+    public void creatEventPast(){
+        UUID id = UUID.fromString("a0d3b612-cde9-417d-8c47-b268cc295e80");
+        LocalDate startDate = LocalDate.of(2024, Month.MARCH, 18);
+        LocalDate endDate = LocalDate.of(2024, Month.MARCH, 20);
+        byte[] images = new byte[0];
+        boolean status = false;
+        boolean payed = false;
+
+        event = new Event(id, "Birthday", "Birthday the Alex Sander", startDate, endDate,"Alex Sander", "Alex Sander", status, payed,
+                "www.github.com", images, Collections.emptyList(), Collections.emptyList(),Collections.emptyList(),Collections.emptyList(), Collections.emptyList()
+                );
+
+    }
+
+    @AfterEach
+    public void setUp(){
+        this.eventRepository.deleteAll();
+    }
+
+    @Test
+    public void save(){
+        var result = serviceEvent.save(event);
+
+        var size = eventRepository.findAll().size();
+
+        Assertions.assertEquals(result.getName(), event.getName());
+        Assertions.assertEquals(1, size, "The quantity of event is different");
+
+        //Assertions.assertTrue(event.isStatus(), "Event not be false");
+        Assertions.assertFalse(event.isStatus(), "Event not be a true");
+
+        //Assertions.assertTrue(event.isPayed(), "Unpaid event ");
+        Assertions.assertFalse(event.isPayed(),"Paid event");
+    }
+
+    @Test
+    public void findById(){
+        serviceEvent.save(event);
+
+        var result = eventRepository.findById(event.getId());
+
+        Assertions.assertEquals(result.get().getId(), event.getId());
+    }
+
+    @Test
+    public void eventList(){
+        serviceEvent.save(event);
+
+        Page<Event>listOfEvents =
+            serviceEvent.list(PageRequest.of(0,50, Sort.Direction.ASC,"Name"));
+
+        Assertions.assertFalse(listOfEvents.getContent().isEmpty());
+        Assertions.assertTrue(listOfEvents.stream().anyMatch(l -> l.getName().equals(event.getName())));
+    }
+
+    @Test
+    public void deleteById(){
+        serviceEvent.save(event);
+
+        Page<Event>listOfEvent =
+            serviceEvent.list(PageRequest.of(0,50, Sort.Direction.ASC,"Name"));
+
+        Assertions.assertFalse(listOfEvent.getContent().isEmpty());
+
+        serviceEvent.deleteById(event.getId());
+
+        listOfEvent =
+            serviceEvent.list(PageRequest.of(0,50, Sort.Direction.ASC, "Name"));
+
+        Assertions.assertTrue(listOfEvent.getContent().isEmpty());
+    }
+}
