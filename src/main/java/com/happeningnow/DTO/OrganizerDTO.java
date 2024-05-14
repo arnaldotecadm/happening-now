248c22ff62551e8558a486c857b6cb3fabbe3900
Alex Sander <sandermeneses2000@hotmail.com>
2024-05-14 15:56:36 +0100

    Inserting swagger into API

---
+++ src/main/java/com/happeningnow/DTO/OrganizerDTO.java
@@ -0,0 +1,9 @@
+//package com.happeningnow.DTO;
+//
+//import lombok.Data;
+//import lombok.experimental.FieldDefaults;
+//
+//@Data
+//@FieldDefaults
+//public class OrganizerDTO {
+//}
--- src/main/java/com/happeningnow/controller/OrganizerController.java
+++ src/main/java/com/happeningnow/controller/OrganizerController.java
@@ -1,37 +1,34 @@
 package com.happeningnow.controller;
 
 import com.happeningnow.model.Organizer;
+import com.happeningnow.repository.OrganizerRepository;
 import com.happeningnow.service.ServiceOrganizer;
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@RequestMapping("/happening-now")
 public class OrganizerController {
 
-    private final ServiceOrganizer serviceOrganizer;
+    private ServiceOrganizer serviceOrganizer;
 
-    public OrganizerController(ServiceOrganizer serviceOrganizer){
+    public OrganizerController(ServiceOrganizer serviceOrganizer, OrganizerRepository organizerRepository){
         this.serviceOrganizer = serviceOrganizer;
     }
 
-    @Operation(summary = "Save", description = "Save organizer", method = "POST", tags = "Organizer")
-    @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Organizer> save(@RequestBody Organizer organizer){
-        return ResponseEntity.status(HttpStatus.CREATED).body(serviceOrganizer.save(organizer));
+    @PostMapping("/save-organizer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Organizer save(@RequestBody Organizer organizer){
+        return serviceOrganizer.save(organizer);
     }
 
-    @Operation(summary = "List", description = "List all organizer", method = "GET", tags = "Organizer")
-    @GetMapping(value = "/list", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Page<Organizer>> list(){
-        return ResponseEntity.ok(serviceOrganizer.list(PageRequest.of(0,50, Sort.Direction.ASC, "Name")));
+//    @GetMapping
+//    public Organizer findById(@PathVariable)
+
+    @GetMapping("/organizers")
+    public Page<Organizer> list(PageRequest pageRequest){
+        return serviceOrganizer.list(pageRequest);
     }
 
 
