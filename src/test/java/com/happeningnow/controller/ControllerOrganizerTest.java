88ac248af16c2b3418877c34334ade4d0fc03082
Alex Sander <sandermeneses2000@hotmail.com>
2024-06-03 15:31:17 +0100

    Creation the Organizer controller test

--- src/test/java/com/happeningnow/controller/ControllerOrganizerTest.java
+++ src/test/java/com/happeningnow/controller/ControllerOrganizerTest.java
@@ -1,19 +1,28 @@
 package com.happeningnow.controller;
 
 import com.happeningnow.model.Organizer;
+import com.happeningnow.repository.OrganizerRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 
@@ -24,15 +33,25 @@ public class ControllerOrganizerTest {
     @Autowired
     private TestRestTemplate resTemplate;
 
-    private Organizer organizer;
+    private Organizer organizer1;
+    private Organizer organizer2;
 
-//    @BeforeEach
-//    public void createOrganizer(){
-//        UUID id = UUID.fromString("a0d3b612-cde9-417d-8c47-b268cc295e80");
-//        organizer = new Organizer(
-//                id,"Alex Sander","Developer","Portugal", Collections.emptyList()
-//        );
-//    }
+//    private OrganizerRepository organizerRepository;
+
+    @BeforeEach
+    public void createOrganizer() {
+        UUID id1 = UUID.fromString("a0d3b612-cde9-417d-8c47-b268cc295e80");
+        organizer1 = new Organizer(
+                id1, "Alex Sander", "Developer", "Portugal", Collections.emptyList()
+        );
+
+        UUID id2 = UUID.fromString("a0d8b613-cde9-417d-9c45-b268cc295e81");
+        organizer2 = new Organizer(
+                id2, "Arnaldo", "Dev", "Porto", Collections.emptyList()
+        );
+
+
+    }
 
 //    @AfterEach
 //    public void setUp(){
@@ -41,9 +60,9 @@ public class ControllerOrganizerTest {
 
     @Test
     @DisplayName("This controller method should save an organizer")
-    public void save(){
+    public void save() {
 
-        HttpEntity<Organizer> saveRequest = new HttpEntity<>(organizer);
+        HttpEntity<Organizer> saveRequest = new HttpEntity<>(organizer1);
         ResponseEntity<Organizer> responseEntity = resTemplate.exchange("/organizer/save",
                 HttpMethod.POST,
                 saveRequest,
@@ -51,15 +70,15 @@ public class ControllerOrganizerTest {
 
         assertThat(responseEntity.getStatusCode().is2xxSuccessful()).isTrue();
         assertThat(responseEntity.getBody()).isNotNull();
-        Assertions.assertEquals("Developer", responseEntity.getBody().getDescription());
         Assertions.assertEquals("Alex Sander", responseEntity.getBody().getName());
+        Assertions.assertEquals("Developer", responseEntity.getBody().getDescription());
         Assertions.assertEquals("Portugal", responseEntity.getBody().getAddress());
     }
 
     @Test
     @DisplayName("This controller method should find an organizer by id")
-    public void findById(){
-        HttpEntity<Organizer> saveRequest = new HttpEntity<>(organizer);
+    public void findById() {
+        HttpEntity<Organizer> saveRequest = new HttpEntity<>(organizer1);
         ResponseEntity<Organizer> saveResponse = resTemplate.exchange("/organizer/save",
                 HttpMethod.POST,
                 saveRequest,
@@ -70,7 +89,7 @@ public class ControllerOrganizerTest {
 
         UUID savedOrganizerId = saveResponse.getBody().getId();
 
-        ResponseEntity<Organizer> responseEntity = resTemplate.exchange("/organizers/{id}",
+        ResponseEntity<Organizer> responseEntity = resTemplate.exchange("/organizer/{id}",
                 HttpMethod.GET,
                 null,
                 Organizer.class,
@@ -85,19 +104,76 @@ public class ControllerOrganizerTest {
         Assertions.assertEquals("Developer", retrievedOrganizer.getDescription());
         Assertions.assertEquals("Portugal", retrievedOrganizer.getAddress());
     }
-//
-//        UUID id = organizer.getId();
-//
-//        when(organizerRepository.findById(id)).thenReturn(Optional.of(organizer));
-//
-//        webTestClient.get().uri("/organizer/{id}", id)
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectBody()
-//                .jsonPath("$.id").isEqualTo(id.toString())
-//                .jsonPath("$.name").isEqualTo("Alex Sander");
-//    }
-//
+
+    @Test
+    @DisplayName("This controller method should find all organizers")
+    public void listOrganizers() {
+        ResponseEntity<Organizer> saveResponse1 = resTemplate.postForEntity("/organizer/save",
+                new HttpEntity<>(organizer1),
+                Organizer.class);
+
+        ResponseEntity<Organizer> saveResponse2 = resTemplate.postForEntity("/organizer/save",
+                new HttpEntity<>(organizer2),
+                Organizer.class);
+
+        assertThat(saveResponse1.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(saveResponse2.getStatusCode()).isEqualTo(HttpStatus.OK); //COLOQUEI CREATED, MAS NÃO FUNCIONA
+
+        ResponseEntity<List<Organizer>> responseEntity = resTemplate.exchange(
+                "/organizer/organizers",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Organizer>>() {});
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isNotNull();
+
+        List<Organizer> organizers = responseEntity.getBody();
+
+        assertThat(organizers).isNotEmpty();
+
+        assertThat(organizers).extracting("name", "description", "address")
+                .contains(tuple("Alex Sander", "Developer", "Portugal"),
+                        tuple("Arnaldo", "Dev", "Porto"));
+    }
+
+
+    @Test
+    @DisplayName("This controller method should delete an organizer by id")
+    public void deleteOrganizerById() {
+        HttpEntity<Organizer> saveRequest = new HttpEntity<>(organizer1);
+        ResponseEntity<Organizer> saveResponse = resTemplate.exchange("/organizer/save",
+                HttpMethod.POST,
+                saveRequest,
+                Organizer.class);
+
+        ResponseEntity<Void> responseEntity = resTemplate.exchange(
+                "/organizer/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class,
+                saveRequest.getBody().getId());
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    @Test
 //    @DisplayName("This controller method do not should find an organizer by id")
 //    public void findById_NotFound(){
