2fd3944c8772e3bb9115589f391059920140e861
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2024-06-30 17:00:01 +0100

    feature - Allow cross origin

--- src/main/java/com/happeningnow/controller/EventController.java
+++ src/main/java/com/happeningnow/controller/EventController.java
@@ -3,11 +3,13 @@ package com.happeningnow.controller;
 
 import com.happeningnow.model.Event;
 import com.happeningnow.service.ServiceEvent;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/event")
 public class EventController extends AbstractController<Event, UUID> {
