aa1e8fd43007b6161801e978c3889ced86d66823
Alex Sander <sandermeneses2000@hotmail.com>
2024-07-23 15:47:49 +0100

    HN-23 add flyway to the project

--- src/main/resources/db/migration/V1__2024_07_15_initial_schema.sql
+++ src/main/resources/db/migration/V1__2024_07_15_initial_schema.sql
@@ -1,9 +1,4 @@
--- public.artist definition
-
--- Drop table
-
--- DROP TABLE artist;
-
+-- TABLE artist;
 CREATE TABLE artist (
 	id uuid NOT NULL,
 	description varchar(255) NOT NULL,
@@ -11,13 +6,7 @@ CREATE TABLE artist (
 	CONSTRAINT artist_pkey PRIMARY KEY (id)
 );
 
-
--- public.category definition
-
--- Drop table
-
--- DROP TABLE category;
-
+-- TABLE category;
 CREATE TABLE category (
 	id uuid NOT NULL,
 	description varchar(255) NOT NULL,
@@ -25,16 +14,10 @@ CREATE TABLE category (
 	CONSTRAINT category_pkey PRIMARY KEY (id)
 );
 
-
--- public."comment" definition
-
--- Drop table
-
--- DROP TABLE "comment";
-
+-- TABLE comment;
 CREATE TABLE comment (
 	id uuid NOT NULL,
-	comment_text varchar(255) NOT NULL,
+	comment varchar(255) NOT NULL,
 	dislikes int4 NOT NULL,
 	likes int4 NOT NULL,
 	title varchar(255) NULL,
@@ -42,13 +25,7 @@ CREATE TABLE comment (
 	CONSTRAINT comment_pkey PRIMARY KEY (id)
 );
 
-
--- public."event" definition
-
--- Drop table
-
--- DROP TABLE "event";
-
+-- TABLE event;
 CREATE TABLE event (
 	id uuid NOT NULL,
 	created_at timestamp NULL,
@@ -64,13 +41,7 @@ CREATE TABLE event (
 	CONSTRAINT event_pkey PRIMARY KEY (id)
 );
 
-
--- public."location" definition
-
--- Drop table
-
--- DROP TABLE "location";
-
+-- TABLE location;
 CREATE TABLE location (
 	id uuid NOT NULL,
 	address varchar(255) NOT NULL,
@@ -79,13 +50,7 @@ CREATE TABLE location (
 	CONSTRAINT location_pkey PRIMARY KEY (id)
 );
 
-
--- public.organizer definition
-
--- Drop table
-
--- DROP TABLE organizer;
-
+-- TABLE organizer;
 CREATE TABLE organizer (
 	id uuid NOT NULL,
 	address varchar(255) NOT NULL,
@@ -94,13 +59,7 @@ CREATE TABLE organizer (
 	CONSTRAINT organizer_pkey PRIMARY KEY (id)
 );
 
-
--- public.event_artist_list definition
-
--- Drop table
-
--- DROP TABLE event_artist_list;
-
+--  TABLE event_artist_list;
 CREATE TABLE event_artist_list (
 	event_list_id uuid NOT NULL,
 	artist_list_id uuid NOT NULL,
@@ -108,13 +67,7 @@ CREATE TABLE event_artist_list (
 	CONSTRAINT fk_event_artist_event FOREIGN KEY (event_list_id) REFERENCES event(id)
 );
 
-
--- public.event_category_list definition
-
--- Drop table
-
--- DROP TABLE event_category_list;
-
+-- TABLE event_category_list;
 CREATE TABLE event_category_list (
 	event_list_id uuid NOT NULL,
 	category_list_id uuid NOT NULL,
@@ -122,13 +75,7 @@ CREATE TABLE event_category_list (
 	CONSTRAINT fk_event_category_event FOREIGN KEY (event_list_id) REFERENCES event(id)
 );
 
-
--- public.event_comment_list definition
-
--- Drop table
-
--- DROP TABLE event_comment_list;
-
+-- TABLE event_comment_list;
 CREATE TABLE event_comment_list (
 	event_id uuid NOT NULL,
 	comment_list_id uuid NOT NULL,
@@ -137,13 +84,7 @@ CREATE TABLE event_comment_list (
 	CONSTRAINT fk_event_comment_event FOREIGN KEY (event_id) REFERENCES event(id)
 );
 
-
--- public.event_organizer_list definition
-
--- Drop table
-
--- DROP TABLE event_organizer_list;
-
+-- TABLE event_organizer_list;
 CREATE TABLE event_organizer_list (
 	event_list_id uuid NOT NULL,
 	organizer_list_id uuid NOT NULL,
@@ -151,26 +92,14 @@ CREATE TABLE event_organizer_list (
 	CONSTRAINT fk_event_organizer_organizer FOREIGN KEY (organizer_list_id) REFERENCES organizer(id)
 );
 
-
--- public.event_tag_list definition
-
--- Drop table
-
--- DROP TABLE event_tag_list;
-
+-- TABLE event_tag_list;
 CREATE TABLE event_tag_list (
 	event_id uuid NOT NULL,
 	tag_list varchar(255) NULL,
 	CONSTRAINT fk_event_tag_event FOREIGN KEY (event_id) REFERENCES event(id)
 );
 
-
--- public.location_event_list definition
-
--- Drop table
-
--- DROP TABLE location_event_list;
-
+-- TABLE location_event_list;
 CREATE TABLE location_event_list (
 	location_id uuid NOT NULL,
 	event_list_id uuid NOT NULL,
